37747a0b4a6313fce2583caf4c163445b4e619cc
utnso <nh.cives@gmail.com>
2020-04-26 19:30:35 -0300

    Se agrega manejo particiones dinamicas y algoritmos de seleccion

--- Broker/Broker.c
+++ Broker/Broker.c
@@ -50,37 +50,142 @@ void iniciar_log(){
 	archivoLog = NULL;
 }
 
-void iniciar_Servicio_Broker(){
+void iniciar_servicio_broker(){
 	int socket = nuevoSocket();
-	uint32_t servidor = asociarSocket(socket, config_File->PUERTO_BROKER);
+	asociarSocket(socket, config_File->PUERTO_BROKER);
 	while(true){
-		esperar_conexion(servidor);
+		esperar_conexion(socket);
 	}
-
-
 }
 
-void esperar_conexion(uint32_t servidor){
-	uint32_t socket = aceptarConexionSocket(servidor);
+void esperar_conexion(int servidor){
+	int socket = aceptarConexionSocket(servidor);
 	pthread_t cliente;
-	pthread_create(&cliente, NULL, (void*)atender,(void*)socket);
+	pthread_create(&cliente, NULL,(void*)atender,(void*)socket);
 	pthread_detach(cliente);
 }
-void atender(){
-
+void atender(int socket){
+	void operacion;
+	while(recv(socket,&operacion,4,MSG_WAITALL) >0){
+		switch(operacion){
+		case NEW_POKEMON:;
+			log_info(logger,"-NEW_POKEMON");
+			aplicar_protocolo_recibir(socket, 1);
+			break;
+		case APPEARED_POKEMON:;
+			log_info(logger,"-APPEARED_POKEMON");
+			aplicar_protocolo_recibir(socket, 3);
+			break;
+		case CATCH_POKEMON:;
+			log_info(logger,"-APPEARED_POKEMON");
+			aplicar_protocolo_recibir(socket, 4);
+			break;
+		case CAUGHT_POKEMON:;
+			log_info(logger,"-CAUGHT_POKEMON");
+			aplicar_protocolo_recibir(socket, 5);
+			break;
+		case GET_POKEMON:;
+			log_info(logger,"-GET_POKEMON");
+			aplicar_protocolo_recibir(socket, 6);
+			break;
+		case LOCALIZED_POKEMON:;
+			log_info(logger,"-LOCALIZED_POKEMON");
+			aplicar_protocolo_recibir(socket, 2);
+			break;
+		}
+	}
 }
 
 void iniciar_estructuras(){
+	//Se reserva la Memoria total del Broker
 	memoria_cache = malloc(config_File->TAMANO_MEMORIA);
 	memset(memoria_cache, '\0', config_File->TAMANO_MEMORIA);
+
 	//SE DEFINE MUTEX PARA DUMP DE MEMORIA CACHE
 	pthread_mutex_init(mutex_memoria_cache, NULL);
 
 }
-
-void algoritmo_primer_ajuste(t_list* lista_particiones){
+void reservar_particion(int tamano, Mensaje msj){
+	if(tamano > config_File->TAMANO_MEMORIA && tamano < config_File->TAMANO_MINIMO_PARTICION){
+		if(strcmp(config_File->ALGORITMO_MEMORIA, PARTICIONES) == 0){
+			reservar_particion_dinamica(tamano, msj);
+		}
+		else if(strcmp(config_File->ALGORITMO_MEMORIA, BUDDY_SYSTEM) == 0){
+			reservar_particion_bs(tamano, msj);
+		}
+		else{/*ERROR CONFIG*/}
+	}
+	else{}
 
 }
-void algoritmo_mejor_ajuste(t_list* lista_particiones){}
-void algoritmo_fifo(t_list* lista_particiones){}
-void algoritmo_lru(t_list* lista_particiones){}
+
+void reservar_particion_dinamica(int tamano, Mensaje mensaje){
+	Particion* particion_libre;
+	int contador_busquedas_fallidas;
+	if(config_File->ALGORITMO_PARTICION_LIBRE == FIRST_FIT){
+		while(cantidad_fallidas <= contador_busquedas_fallidas || particion_libre != NULL){
+		particion_libre = algoritmo_primer_ajuste(tamano);
+		if(particion_libre == NULL){contador_busquedas_fallidas++;}
+		}
+		if(particion_libre == NULL && cantidad_fallidas == contador_busquedas_fallidas){
+			compactacion();
+			cantidad_fallidas = 0;
+			while(cantidad_fallidas <= contador_busquedas_fallidas || particion_libre != NULL){
+				particion_libre = algoritmo_primer_ajuste(tamano);
+				if(particion_libre == NULL){contador_busquedas_fallidas++;}
+			}
+			if(particion_libre == NULL && cantidad_fallidas == contador_busquedas_fallidas){
+				eliminar_particion();
+			}
+		}
+	}
+	else if(config_File->ALGORITMO_PARTICION_LIBRE == BEST_FIT){
+		while(cantidad_fallidas <= contador_busquedas_fallidas){
+		particion_libre = algoritmo_mejor_ajuste(tamano);
+		if(particion_libre == NULL && cantidad_fallidas ){contador_busquedas_fallidas++;}
+		}
+		if(particion_libre == NULL && cantidad_fallidas == contador_busquedas_fallidas){
+			compactacion();
+		}
+
+	}
+	free(particion_libre);
+}
+
+void reservar_particion_bs(int tamano, Mensaje mensaje){
+
+}
+/*
+Particion* algoritmo_primer_ajuste(int tamano){
+	int cantidad_particiones = list_size(lista_particiones);
+	for(int i = 1; i<= cantidad_particiones; i++){
+		Particion* particion = list_get(lista_particiones, i);
+		if(particion != NULL && particion->tamano >= tamano){return particion;}
+	}
+	return NULL;
+}
+*/
+Particion* algoritmo_primer_ajuste(int tamano){
+	_Bool particion_libre(Particion* particion){return particion->libre && particion->tamano >= tamano;}
+	return list_find(lista_particiones, (void*)particion_libre);
+}
+/*
+void algoritmo_mejor_ajuste(int tamano){
+	int cantidad_particiones = list_size(lista_particiones);
+	t_list* lista_particiones_ordenadas
+
+}
+*/
+Particion* algoritmo_mejor_ajuste(int tamano){
+	_Bool ordenar(Particion* particion1, Particion* particion2){return particion1->tamano < particion2->tamano;}
+	t_list* lista_ordenada = lista_particiones;
+	list_sort(lista_ordenada, (void*)ordenar);
+	_Bool particion_libre(Particion* particion){return particion->libre && particion->tamano >= tamano;}
+	return list_find(lista_ordenada, (void*)particion_libre);
+	free(lista_ordenada);
+}
+
+void algoritmo_fifo(int tamano){}
+void algoritmo_lru(int tamano){}
+void compactacion(){}
+void eliminar_particion(){}
--- Broker/Broker.h
+++ Broker/Broker.h
@@ -12,27 +12,30 @@
 #include <commons/config.h>
 #include <commons/string.h>
 #include <commons/collections/list.h>
-#include <digiCommons/src/sockets.h>
+#include <../digiCommons/src/sockets.h>
+#include <../digiCommons/src/mensajeria.h>
+#include <../digiCommons/src/protocolos_comunicacion.h>
 #include <pthread.h>
 #include <signal.h>
 
-typedef struct{
+typedef struct Mensaje{
 	int id_msj;
-	int id_cola_msj;
-	t_list* ALGORITMO_MEMORIA;
-	char* ALGORITMO_REEMPLAZO;
-	char* ALGORITMO_PARTICION_LIBRE;
-	char* IP_BROKER;
-	int PUERTO_BROKER;
-	int FRECUENCIA_COMPACTACION;
-	char* LOG_FILE;
+	int tipo_msj;
+	t_list* lista_suscriptores;
+	t_list* lista_suscriptores_ACK;
 } Mensaje;
+
+typedef struct Particion{
+	int tamano;
+	_Bool libre;
+	//void* puntero;
+}Particion;
 
 typedef struct{
 
 }Suscriptor;
 
-typedef struct{
+typedef struct ConfigFile{
 	int TAMANO_MEMORIA;
 	int TAMANO_MINIMO_PARTICION;
 	char* ALGORITMO_MEMORIA;
@@ -53,14 +56,15 @@ typedef struct{
 	char* nombre_pokemon;
 	uint32_t  posicion_x;
 	uint32_t posicion_y;
-}NEW_POKEMON;
+	uint32_t cantidad;
+}tamano_NEW_POKEMON;
 
 typedef struct{
 	uint32_t largo_nombre;
 	char* nombre_pokemon;
 	uint32_t cantidad;
 	t_list* lista_posiciones;
-}LOCALIZED_POKEMON;
+}tamano_LOCALIZED_POKEMON;
 
 typedef struct{
 	uint32_t posicion_x;
@@ -70,30 +74,55 @@ typedef struct{
 typedef struct{
 	uint32_t largo_nombre;
 	char* nombre_pokemon;
-}GET_POKEMON;
+}tamano_GET_POKEMON;
 
 typedef struct{
 	uint32_t largo_nombre;
 	char* nombre_pokemon;
 	uint32_t posicion_x;
 	uint32_t posicion_y;
-}APPEARED_POKEMON;
+}tamano_APPEARED_POKEMON;
 
 typedef struct{
 	uint32_t largo_nombre;
 	char* nombre_pokemon;
 	uint32_t posicion_x;
 	uint32_t posicion_y;
-}CATCH_POKEMON;
+}tamano_CATCH_POKEMON;
 
 typedef struct{
 	uint32_t atrapo_pokemon;
-}CAUGHT_POKEMON;
+}tamano_sCAUGHT_POKEMON;
 
 void* memoria_cache;
 pthread_mutex_t mutex_memoria_cache;
 ConfigFile* config_File;
 t_log* logger;
 t_list* lista_particiones;
+t_list* cola_new_pokemon;
+t_list* cola_localized_pokemon;
+t_list* cola_get_pokemon;
+t_list* cola_appeared_pokemon;
+t_list* cola_catch_pokemon;
+char FIRST_FIT[2]= "FF";
+char BEST_FIT[2] = "BF";
+char PARTICIONES[11] = "PARTICIONES";
+char BUDDY_SYSTEM[2] = "BS";
+int cantidad_fallidas = config_File->FRECUENCIA_COMPACTACION;
+
+
+void leer_configFile();
+void iniciar_log();
+void iniciar_servicio_broker();
+void esperar_conexion(int servidor);
+void atender(int socket);
+void iniciar_estructuras();
+void reservar_particion(int tamano, Mensaje msj);
+void reservar_particion_dinamica(int tamano, Mensaje mensaje);
+void reservar_particion_bs(int tamano, Mensaje mensaje);
+Particion* algoritmo_primer_ajuste(int tamano);
+Particion* algoritmo_mejor_ajuste(int tamano);
+void compactacion();
+void eliminar_particion();
 
 #endif /* BROKER_H_ */
