1fc59aac2a8a23a4f5f78895be257a7abde5bbb3
NicoCives <nh.cives@gmail.com>
2020-07-24 21:32:52 -0300

    Se agregan archivos Makefile

---
+++ GameCard/Debug/GameCard.d
@@ -0,0 +1,19 @@
+GameCard.o: ../GameCard.c ../GameCard.h ../Inicializacion.h \
+ ../src/protocolos_comunicacion.h ../src/sockets.h ../src/mensajeria.h \
+ ../src/mensajeria.h ../MetodosGC.h ../src/sockets.h
+
+../GameCard.h:
+
+../Inicializacion.h:
+
+../src/protocolos_comunicacion.h:
+
+../src/sockets.h:
+
+../src/mensajeria.h:
+
+../src/mensajeria.h:
+
+../MetodosGC.h:
+
+../src/sockets.h:
---
+++ GameCard/Debug/Inicializacion.d
@@ -0,0 +1,19 @@
+Inicializacion.o: ../Inicializacion.c ../Inicializacion.h ../GameCard.h \
+ ../src/protocolos_comunicacion.h ../src/sockets.h ../src/mensajeria.h \
+ ../src/mensajeria.h ../src/sockets.h ../MetodosGC.h
+
+../Inicializacion.h:
+
+../GameCard.h:
+
+../src/protocolos_comunicacion.h:
+
+../src/sockets.h:
+
+../src/mensajeria.h:
+
+../src/mensajeria.h:
+
+../src/sockets.h:
+
+../MetodosGC.h:
